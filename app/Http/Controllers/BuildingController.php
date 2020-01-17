220593956ac2113d7ff536a7672754fe5dab86ca
vanfsy <vanfsy1225@gmail.com>
2020-01-17 11:42:31 +0900

    building download

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -296,7 +296,7 @@ class BuildingController extends Controller
         $building = Building::find($id);
         $file_name = $building->file_name;
 //        $file_path = asset('upload/' . $file_name);
-        $file_path = 'tems2/public/uploads/' . $file_name;
+        $file_path = '/tems2/public/uploads/' . $file_name;
 
         if (file_exists($file_path)) {
             return Response::download($file_path, $file_name, [
