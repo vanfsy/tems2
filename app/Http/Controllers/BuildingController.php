c0b66cc5e324d453e8befd20c624ba2cb07eb4ab
vanfsy <vanfsy1225@gmail.com>
2020-02-11 17:15:05 +0900

    building search check

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -23,7 +23,7 @@ class BuildingController extends Controller
     public function index(Request $request)
     {
         $query_count = 0;
-        $query = $this->model->query();
+        $query = Building::query();
         $query = DB::table('buildings');
 //        if (!empty($request->request)) {
 //            foreach ($this->searchAction($request->request) as $k => $v) {
